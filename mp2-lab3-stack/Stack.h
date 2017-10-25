38e628118670bc2e9d2ccda62ea246ef419f98c4
unknown <logvinenko.a@WS-6K-102-A02.ccam.cmc.unn.net>
2017-10-25 09:57:17 +0300

    C

--- mp2-lab3-stack/Stack.h
+++ mp2-lab3-stack/Stack.h
@@ -1,12 +1,66 @@
+
 template <class T>
 class TStack {
 	int maxsize;
 	int size;
 	T* array;
 public:
-	TStack (int _msize=200) {
+	TStack<T> (int _msize=200) {
 		array=new T[_msize];
 		maxsize=_msize;
 		size=0;
+	}
+	~TStack<T>() {
+		delete[] array;
+	}
+	TStack<T> (const TStack<T>& TS) {
+		size=TS.size;
+		maxsize=TS.maxsize;
+		array=new T[maxsize];
+		for (int i=0; i<size; i++)
+			array[i]=TS.array[i];
+	}
+	TStack<T>& operator=(const TStack& TS) {
+		if(this!=&TS) {
+			if(maxsize!=TS.maxsize) {
+				delete[] array;
+				maxsize=TS.maxsize;
+				array=new T[maxsize];
+			}
+			size=TS.size;
+			for(int i=0; i<size; i++)
+				array[i]=TS.array[i];
+		}
+		return (*this);
+	}
+
+	int IsFull() {
+		if(size==maxsize)
+			return 1;
+		else
+			return 0;
+	}
+	int IsEmpty() {
+		if(size==0)
+			return 0;
+		else 
+			return 1;
+	}
+	T Top(){
+		if(IsEmpty())
+			throw -1;
+		return array[size-1];
+	}
+	T Pop() {
+		if(IsEmpty())
+			throw -1;
+		size--;
+		return array[size];
+	}
+	void Push(const T& el){
+		if(IsFull())
+			throw -1;
+		array[size]=el;
+		size++;
 	}
 };
